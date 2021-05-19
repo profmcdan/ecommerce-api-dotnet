87fd56610912e9e309b50737e4490d8f2734bfeb
Daniel Ale <danielale9291@gmail.com>
2021-05-19 11:52:50 +0100

    bg -- fix error on sql for create

--- Discount.API/Repositories/DiscountRepository.cs
+++ Discount.API/Repositories/DiscountRepository.cs
@@ -41,7 +41,7 @@ namespace Discount.API.Repositories
             await using var connection =
                 new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
             var affected = await connection.ExecuteAsync(
-                "INSERT INTO Coupon (ProductName,, Description, Amount) VALUES (@ProductName, @Description, @Amount)",
+                "INSERT INTO Coupon (ProductName, Description, Amount) VALUES (@ProductName, @Description, @Amount)",
                 new {ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount});
             return affected != 0;
         }
